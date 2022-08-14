f206f7354f7474235de5924e9ad5fde7fff4eb4e
murase <murase@hp-pc>
2022-08-15 00:58:45 +0900

    PUSH型で転送しているプログラムを，PULL型で転送するようにリファクタリング

--- src/main/java/resources/Shipping.java
+++ src/main/java/resources/Shipping.java
@@ -1,17 +1,14 @@
 package resources;
 import values.Item;
-import values.ItemHandling;
-
-import java.util.*;
 
 public class Shipping {
-	Item value;
-
-	public void updateHandling(ItemHandling handling) {
-		value = new Item(handling.getName(), handling.getShippingNum());
+	Handling handling;
+	
+	public Shipping(Handling handling) {
+		this.handling = handling;
 	}
 
 	public Item getValue() {
-		return value;
+		return new Item(handling.getValue().getName(), handling.getValue().getShippingNum());
 	}
 }
